bf2852394ba0db5222343a15462dee67fe18ea20
Michel Muñoz <michel@febos.cl>
2020-04-19 13:14:39 -0400

    normalizacion archivos env
    Took 21 minutes

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 import state from "./state"
 import getters from "./getters"
 import mutations from "./mutations"
 import actions from "./actions"
+
 
 import moduloUsuario from "../febos/global/usuario/store/moduloUsuario";
 
@@ -18,5 +20,10 @@ export default new Vuex.Store({
     modules:{
       usuario:moduloUsuario
     },
+    plugins: [createPersistedState({
+      key:`${process.env.VUE_APP_AMBIENTE}/${process.env.VUE_APP_PORTAL}`,
+      overwrite: false,
+      fetchBeforeUse: false
+    })],
     strict: process.env.NODE_ENV !== 'production'
 })
