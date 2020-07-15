2f8e00d9822f7ec579a2814cc3d0ac94e9fcbb96
Claudio Miranda <cronosunder@gmail.com>
2020-07-15 18:46:10 -0400

    Interceptor para identificar portal (De momento solo asigna portal.febos.cl)

--- src/febos/servicios/clienteFebosAPI.js
+++ src/febos/servicios/clienteFebosAPI.js
@@ -6,18 +6,18 @@ let config = {
 };
 
 const clienteFebosAPI = axios.create(config);
-const noLoguearEstosLambdas=["io.usuario.latido"];
+const noLoguearEstosLambdas = ["io.usuario.latido"];
 
 const authInterceptor = config => {
-  let storage=JSON.parse(localStorage.getItem(`${process.env.VUE_APP_AMBIENTE}/${process.env.VUE_APP_PORTAL}`));
-  config.headers.token=storage.Usuario.token;
-  try{
+  let storage = JSON.parse(localStorage.getItem(`${process.env.VUE_APP_AMBIENTE}/${process.env.VUE_APP_PORTAL}`));
+  config.headers.token = storage.Usuario.token;
+  try {
     config.headers.empresa = storage.Empresas.empresa.iut;
-  }catch(e){
+  } catch (e) {
     console.log("llamada sin rut de empresa");
   }
   //config.headers.Accept='application/json';
-  config.headers['Content-Type']='application/json';
+  config.headers['Content-Type'] = 'application/json';
   return config;
 };
 
@@ -26,11 +26,11 @@ const revalidarSesion = () => {
 }
 
 const ocultarClaves = (parametros) => {
-  const parametrosTipoClave = ['clave','password'];
+  const parametrosTipoClave = ['clave', 'password'];
   for (let parametro in parametros) {
     if (parametros.hasOwnProperty(parametro)) {
-      if(parametrosTipoClave.includes(parametro)){
-          parametros[parametro]='*******';
+      if (parametrosTipoClave.includes(parametro)) {
+        parametros[parametro] = '*******';
       }
     }
   }
@@ -47,32 +47,42 @@ const loggerInterceptor = config => {
   }
   // limpieza de variables residuales
   delete llamadaAPI.parametros.operacionId;
-  let verbos=['post','get','delete','put','patch','head','common'];
-  verbos.forEach(function(verbo){
+  let verbos = ['post', 'get', 'delete', 'put', 'patch', 'head', 'common'];
+  verbos.forEach(function (verbo) {
     delete llamadaAPI.cabeceras[verbo];
   })
 
-  llamadaAPI.parametros=ocultarClaves(llamadaAPI.parametros);
-  if(!noLoguearEstosLambdas.includes(llamadaAPI.operacionId)) {
+  llamadaAPI.parametros = ocultarClaves(llamadaAPI.parametros);
+  if (!noLoguearEstosLambdas.includes(llamadaAPI.operacionId)) {
     console.log(`>> Llamada API (request): ${llamadaAPI.operacionId}`, llamadaAPI);
   }
   return config;
 }
 
 
+
+const portalInterceptor = config => {
+  config.params = config.params?{...{dominioPortal:`${process.env.VUE_APP_DOMINIO_DEFAULT}`},...config.params}:{dominioPortal:`${process.env.VUE_APP_DOMINIO_DEFAULT}`}
+  //params.push(`dominioPortal=${process.env.VUE_APP_DOMINIO_DEFAULT}`);
+  console.log("config",config);
+  return config;
+}
+
+
+clienteFebosAPI.interceptors.request.use(portalInterceptor);
 clienteFebosAPI.interceptors.request.use(authInterceptor);
 clienteFebosAPI.interceptors.request.use(loggerInterceptor);
 
 clienteFebosAPI.interceptors.response.use(
   response => {
-    let opracionId=response.config.operacionId
-    let respuestaAPI={
-        headers:response.headers,
-        httpCode:response.status,
-        data:response.data
-      }
-      if(response.data.codigo>=10)revalidarSesion();
-    if(!noLoguearEstosLambdas.includes(response.config.operacionId)) {
+    let opracionId = response.config.operacionId
+    let respuestaAPI = {
+      headers: response.headers,
+      httpCode: response.status,
+      data: response.data
+    }
+    if (response.data.codigo >= 10) revalidarSesion();
+    if (!noLoguearEstosLambdas.includes(response.config.operacionId)) {
       console.log(">> Respuesta de API (response): " + opracionId, respuestaAPI);
     }
     return response;
@@ -83,8 +93,8 @@ clienteFebosAPI.interceptors.response.use(
   }
 );
 
-clienteFebosAPI.queryParams=function(json){
-  let params=[];
+clienteFebosAPI.queryParams = function (json) {
+  let params = [];
   for (let llave in json) {
     if (json.hasOwnProperty(llave)) {
       params.push(`${llave}=${json[llave]}`);
