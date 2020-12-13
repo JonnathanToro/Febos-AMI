9e89c7ff04d5e43dde57a1fccc9831c1b6ae0691
Letty Villamizar <leettyvillamizar@egmail.com>
2020-12-13 15:32:15 -0300

    feature: ajustes en la vista de expedientes, ajuste de filtros Vue

--- src/febos/chile/dnt/vistas/Files.vue
+++ src/febos/chile/dnt/vistas/Files.vue
@@ -1,118 +1,123 @@
 <template lang="html">
-  <div id="list-dnt">
+  <div>
     <vs-row vs-w="12" class="list-wrapper title-columns">
       <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="1" vs-sm="4" vs-xs="12">
         <b>Número</b>
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="1" vs-sm="4" vs-xs="12">
+      <vs-col vs-type="flex" vs-justify="left" vs-align="center" vs-lg="1" vs-sm="4" vs-xs="12">
         <b>Estado</b>
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+      <vs-col vs-type="flex" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
         <b>Enviado por</b>
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+      <vs-col vs-type="flex" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
         <b>Razón social receptor</b>
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+      <vs-col vs-type="flex" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
         <b>Último aprobador</b>
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+      <vs-col vs-type="flex" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
         <b>Actualización</b>
       </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+      <vs-col vs-type="flex" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
         <b>Creación</b>
       </vs-col>
     </vs-row>
-    <vs-row
-      vs-w="12"
-      :key="file.febosId"
-      v-for="file in dntByFiles"
-      class="content-rows list-wrapper"
-    >
-      <vs-col vs-type="block" class="numero-file" vs-lg="1" vs-sm="4" vs-xs="12">
-        <div>
-           # <br>
-        </div>
-        <div>
-          {{ file.numero }}
-        </div>
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="1" vs-sm="4" vs-xs="12">
-        <vs-icon
-          v-if="file.estado === 3"
-          icon="schedule"
-          size="medium"
-          bg="#43C3B9"
-          class="state-icon"
-          color="white"/>
-        <vs-icon
-          v-if="file.estado === 4"
-          icon="thumb_up"
-          size="medium"
-          bg="#14AA59"
-          class="state-icon"
-          color="white"/>
-        <vs-icon
-          v-if="file.estado === 5"
-          icon="thumb_down"
-          size="medium"
-          bg="#CE4B4B"
-          class="state-icon"
-          color="white"/>
-        <vs-icon
-          v-if="file.estado === 6"
-          icon="close"
-          size="medium"
-          bg="#A04E4E"
-          class="state-icon"
-          color="white"/>
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
-        {{ file.solicitanteNombre }}
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
-        {{ file.receptorSucursalNombre }}
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
-        {{ file.receptorContactoNombre }}
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
-        {{ file.fechaActualizacion }}
-      </vs-col>
-      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
-        {{ file.fechaEntrega }}
-      </vs-col>
-      <vs-col
-        vs-type="flex"
-        vs-justify="center"
-        vs-align="center"
-        class="aditional-info"
-        vs-lg="12"
-        vs-sm="12"
-        vs-xs="12"
+    <div id="list-dnt" style="padding-top: 56px;">
+      <vs-row
+        vs-w="12"
+        :key="file.febosId"
+        v-for="file in dntByFiles"
+        class="content-rows list-wrapper"
       >
-        <span class="pill-info" v-if="file.emisorInfoAdicional">
+        <vs-col vs-type="block" class="numero-file" vs-lg="1" vs-sm="4" vs-xs="12">
+          <div>
+            # <br>
+          </div>
+          <div>
+            {{ file.numero }}
+          </div>
+        </vs-col>
+        <vs-col vs-type="block" vs-justify="left" vs-align="center" vs-lg="1" vs-sm="4" vs-xs="12">
+          <vs-icon
+            title="En curso"
+            v-if="file.estado === 3"
+            icon="schedule"
+            size="medium"
+            bg="#43C3B9"
+            class="state-icon"
+            color="white"/>
+          <vs-icon
+            title="Aprobado"
+            v-if="file.estado === 4"
+            icon="thumb_up"
+            size="medium"
+            bg="#14AA59"
+            class="state-icon"
+            color="white"/>
+          <vs-icon
+            title="Rechazado"
+            v-if="file.estado === 5"
+            icon="thumb_down"
+            size="medium"
+            bg="#CE4B4B"
+            class="state-icon"
+            color="white"/>
+          <vs-icon
+            title="Anulado"
+            v-if="file.estado === 6"
+            icon="close"
+            size="medium"
+            bg="#A04E4E"
+            class="state-icon"
+            color="white"/>
+        </vs-col>
+        <vs-col vs-type="block" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+          {{file.solicitanteNombre| capitalize }}
+        </vs-col>
+        <vs-col vs-type="block" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+          {{ file.receptorSucursalNombre | capitalize }}
+        </vs-col>
+        <vs-col vs-type="block" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+          {{ file.receptorContactoNombre | capitalize }}
+        </vs-col>
+        <vs-col vs-type="block" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+          {{ file.fechaActualizacion }}
+        </vs-col>
+        <vs-col vs-type="block" vs-justify="left" vs-align="center" vs-lg="2" vs-sm="4" vs-xs="12">
+          {{ file.fechaEntrega }}
+        </vs-col>
+        <vs-col
+          vs-type="block"
+
+          class="aditional-info"
+          vs-lg="12"
+          vs-sm="12"
+          vs-xs="12"
+        >
+        <span class="pill-info" v-if="file.emisorInfoAdicional" title="División a la que pertenece">
           {{file.emisorInfoAdicional}}
         </span>
-        <span class="pill-info" v-if="file.privado === 'Y'">
+          <span class="pill-info" v-if="file.privado === 'Y'" title="Es un archivo privado">
           <vs-icon icon="lock"></vs-icon>
         </span>
-        <span class="pill-info" :v-if="file.numeroInt">
+          <span class="pill-info" v-if="file.numeroInt" title="Tipo de documento">
           {{findTypeDocument(file.numeroInt)}}
         </span>
-        <span class="pill-info" :v-if="file.numeroInt">
-          Tiempo 1d 4h
+          <span class="pill-info" v-if="file.plazo" title="tiempo Transcurrido">
+          {{translateTime(file.plazo, true)}}
         </span>
-        <vs-icon
-          class="actions"
-          icon="bubble_chart"
-          size="medium"
-          bg="#E4E4E4"
-          round
-          color="gray"
-        />
-      </vs-col>
-    </vs-row>
+          <vs-icon
+            class="actions"
+            icon="bubble_chart"
+            size="medium"
+            bg="#E4E4E4"
+            round
+            color="gray"
+          />
+        </vs-col>
+      </vs-row>
+    </div>
   </div>
 </template>
 <script>
@@ -123,9 +128,7 @@ import FindTypeDocumentMixin from '@/febos/chile/dnt/mixins/FindTypeDocumentMixi
 
 export default {
   data() {
-    return {
-      holi: ''
-    };
+    return {};
   },
   mixins: [FiltersDntMixin, FindTypeDocumentMixin],
   watch: {
@@ -150,7 +153,42 @@ export default {
   methods: {
     ...mapActions('Dnts', [
       'listDocuments'
-    ])
+    ]),
+    translateTime: (time, abr) => {
+      const seconds = time * 60;
+      const numdays = Math.floor(seconds / 86400);
+      const numhours = Math.floor((seconds % 86400) / 3600);
+      const numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
+
+      if (numdays !== 0) {
+        if (abr) {
+          return `${numdays } d ${ numhours } hrs ${ numminutes } min `;
+        }
+        return `${numdays } dias ${ numhours } horas ${ numminutes } minutos `;
+      }
+
+      if (numdays === 0 && numhours !== 0) {
+        if (abr) {
+          return `${numhours } hrs ${ numminutes } min `;
+        }
+        return `${numhours } horas ${ numminutes } minutos `;
+      }
+
+      if (numdays === 0 && numhours === 0 && numminutes !== 0) {
+        if (abr) {
+          return `${numminutes } min `;
+        }
+        return `${numminutes } minutos `;
+      }
+
+      if (numminutes === 0) {
+        if (abr) {
+          return `${numminutes } min`;
+        }
+        return `${numminutes } minutos`;
+      }
+      return numminutes;
+    }
   },
   mounted() {
     const view = this.$route.params.vista;
@@ -162,7 +200,7 @@ export default {
       orden: '-fechaCreacion',
       itemsPorPagina: 200,
       // TODO agregar bien los filtros
-      filtros: filters
+      filtros: filters.concat('|fechaCreacion:2020-06-13--2020-12-13')
     });
     console.log('VIEW', view);
   }
@@ -175,8 +213,13 @@ export default {
 }
 
 .title-columns {
-  padding: 10px 0;
+  padding: 16px 10px;
   margin-bottom: 10px;
+  background: white;
+  position: fixed;
+  width: 81% !important;
+  z-index: 10;
+  box-shadow: -1px 6px 12px 0 #80808075;
 }
 
 .content-rows {
@@ -195,17 +238,19 @@ export default {
   border-radius: 10px;
   font-size: 12px;
   margin-right: 20px;
+  cursor:pointer;
 }
 
 .actions {
   float:right;
   position: absolute;
   right: 10px;
+  bottom: 10px;
   cursor: pointer;
 }
 .aditional-info {
   padding: 4px 0;
-  margin: 0 auto 0 auto;
+  margin-left: 16% !important;
   position: relative;
 }
 
@@ -213,5 +258,8 @@ export default {
   border-radius: 25px;
   font-size: 22px !important;
   padding: 5px;
+}
+.tooltip-inline {
+  display: inline !important;
 }
 </style>
