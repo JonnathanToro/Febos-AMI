2c61570688a7fdbd07a1f476280202bc1b359dda
Michel Muñoz <michel@febos.cl>
2021-01-10 23:14:11 -0300

    deploy para ED
    Took 2 hours 15 minutes

---
+++ deployED.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+AMBIENTE=$1
+PORTAL=$2
+
+ambientes_disponibles=("desarrollo" "pruebas" "certificacion" "produccion")
+portales_disponibles=("cloud")
+function contains() {
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $#;i++)) {
+        if [ "${!i}" == "${value}" ]; then
+            echo "y"
+            return 0
+        fi
+    }
+    echo "n"
+    return 1
+}
+
+if [ $(contains "${ambientes_disponibles[@]}" "$AMBIENTE") == "n" ]; then
+  echo "Ambiente no valido"
+  exit 0
+fi
+
+if [ $(contains "${portales_disponibles[@]}" "$PORTAL") == "n" ]; then
+  echo "Portal no valido"
+  exit 0
+fi
+
+COMANDO="vue-cli-service build --mode ed.$AMBIENTE.$PORTAL"
+$COMANDO
+echo "* Subiendo Portal"
+aws s3 cp dist/ "s3://portal.escritoriodigital.cl/$AMBIENTE/$PORTAL/" --only-show-errors --recursive
+echo "* Borrando Cache"
+aws cloudfront create-invalidation --distribution-id EK96US4D2LAMT --paths "/$AMBIENTE/*" > /dev/null 2>&1
+echo "Listo!"
+
