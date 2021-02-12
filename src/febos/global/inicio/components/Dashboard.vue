10835d553bfdc021397460cd897207ec576feb6b
Claudio Miranda Pizarro <cronosunder@gmail.com>
2021-02-12 11:01:10 -0300

    Intento de indicadores globales

--- src/febos/global/inicio/components/Dashboard.vue
+++ src/febos/global/inicio/components/Dashboard.vue
@@ -28,6 +28,37 @@
         </div>
       </div>
     </div>
+    <div class="centrador">
+      <h4 :style="titleStyles" :class="titleClasses">Mis documentos</h4>
+    </div>
+    <div class="container">
+      <div class="centrador">
+        <div class="modulo mr-5 mt-5 pt-5">
+          <div class="nombre-modulo">
+            {{personalIndicators.finalizados || 0}} Finalizados
+          </div>
+          <vs-icon icon="folder_open" size="75px" color="#ffffff" class="mt-2"></vs-icon>
+        </div>
+        <div class="modulo mr-5 mt-5 pt-5">
+          <div class="nombre-modulo">
+            {{personalIndicators.pendientes || 0}} Pendientes
+          </div>
+          <vs-icon icon="folder_open" size="75px" color="#ffffff" class="mt-2"></vs-icon>
+        </div>
+        <div class="modulo mr-5 mt-5 pt-5">
+          <div class="nombre-modulo">
+            {{personalIndicators.enCopia || 0}} en copia
+          </div>
+          <vs-icon icon="folder_open" size="75px" color="#ffffff" class="mt-2"></vs-icon>
+        </div>
+        <div class="modulo mr-5 mt-5 pt-5">
+          <div class="nombre-modulo">
+            {{personalIndicators.anulado || 0}} anulados
+          </div>
+          <vs-icon icon="folder_open" size="75px" color="#ffffff" class="mt-2"></vs-icon>
+        </div>
+      </div>
+    </div>
   </vx-card>
 </template>
 <script>
@@ -81,22 +112,5 @@ export default {
 
 .nombre-modulo {
   font-weight: bold;
-}
-
-.modulo-deshabilitado {
-  background-color: #ccc !important;
-}
-
-.modulo-deshabilitado:hover {
-  background-color: #ccc !important;
-  box-shadow: 3px 3px 10px #ccc;
-  cursor: default;
-  text-shadow: none !important;
-}
-
-.modulo-actual {
-  position: relative;
-  right: -20px;
-  bottom: -5px;
 }
 </style>
