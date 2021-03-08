cb511bd6235270a6abef66fe424f1873cc336280
Letty Villamizar <leettyvillamizar@gmail.com>
2021-03-08 16:04:49 -0300

    feature: order in list of users/groups and lists in forms

--- src/febos/global/empresas/store/moduloEmpresasGetters.js
+++ src/febos/global/empresas/store/moduloEmpresasGetters.js
@@ -11,5 +11,7 @@ export const iut = (state) => state.empresa.iut;
 
 export const companies = (state) => state.empresas;
 
-export const usersCompany = (state) => state.usersCompany;
-export const groupsCompany = (state) => state.groupsCompany;
+export const usersCompany = (state) => (state.usersCompany || [])
+  .sort(({ nombre: a }, { nombre: b }) => a.localeCompare(b));
+export const groupsCompany = (state) => (state.groupsCompany || [])
+  .sort(({ nombre: a }, { nombre: b }) => a.localeCompare(b));
