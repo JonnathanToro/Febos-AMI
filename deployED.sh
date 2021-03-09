6f46a6464d0a20737ed57112dac4ecdd8592e80a
Letty Villamizar <leettyvillamizar@gmail.com>
2021-03-09 12:37:19 -0300

    updated sh file

--- deployED.sh
+++ deployED.sh
@@ -51,7 +51,7 @@ esac
 
 if [ -n "$DISTRIBUTION_ID" ]; then
   echo "* Borrando Cache"
-  aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/$ENVIRONMENT/*" > /dev/null 2>&1
+  aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/$PORTAL/*" > /dev/null 2>&1
   $CLEAR_COMMAND
 else
   echo "No se tiene id de distribución para este ambiente, no se puede borrar el cache"
