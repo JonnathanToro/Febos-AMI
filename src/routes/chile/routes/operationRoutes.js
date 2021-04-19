51e2c841726723541ae0b7805153cec44ad59f89
Letty Villamizar <leettyvillamizar@gmail.com>
2021-04-19 14:19:14 -0400

    fix: fix route character

--- src/routes/chile/routes/operationRoutes.js
+++ src/routes/chile/routes/operationRoutes.js
@@ -3,6 +3,6 @@ export default [
     path: 'administracion/usuarios',
     name: 'users-management',
     meta: { permiso: 'WB001', requiereLogin: true }, // TODO set permission
-    component: () => import('@/febos/global/management/vistas/UsersManagment.vue')
+    component: () => import('@/febos/global/management/vistas/UsersManagement.vue')
   },
 ];
